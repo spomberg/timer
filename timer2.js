e975915099272b329f430778e76d3c81ee2146ba
Marcos Spomberg <marcosspomberg@gmail.com>
2021-11-13 17:45:30 +0000

    Fix bugs - separated SIGINT function from mainfunction to prevent it from running it multiple times

--- timer2.js
+++ timer2.js
@@ -1,16 +1,14 @@
 const readline = require('readline');
 const { stdin: input, stdout: output } = require('process');
-const { time } = require('console');
 const rl = readline.createInterface({ input, output });
 
 const timer = () => {
-  rl.question("Please enter your input\n", (input) => {
+  rl.question("Please input your command\n", (input) => {
     if (input === 'b') {
       process.stdout.write('\x07');
       timer();
-    }
-    if (input >= 1 && input <= 9) {
-      console.log(`Setting timer for ${input} seconds`);
+    }else if (input >= 1 && input <= 9) {
+      console.log(`Setting timer for ${input} second`);
       setTimeout(() => {
         process.stdout.write('\x07');
         timer();
@@ -19,11 +17,11 @@ const timer = () => {
       console.log(`Invalid input`);
       timer();
     }
-    rl.on('SIGINT', () => {
-      console.log(`Thanks for using me, ciao!`);
-      rl.close();
-    });
   });
 };
 
 timer();
+rl.on('SIGINT', () => {
+  console.log(`Thanks for using me, ciao!`);
+  rl.close();
+});
